2a52e531aa96444040fd3ff13373f5edab69fdc2
NielsTRS <trsniels@gmail.com>
2023-12-15 14:48:51 +0100

    Ajout option a run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 make_option=false
+run_option=false
 
-while getopts ":m" opt; do
+while getopts ":mr" opt
+do
   case $opt in
     m)
       make_option=true
+      ;;
+    r)
+      run_option=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -25,5 +30,17 @@ then
   make
 fi
 
-./arm_simulator --gdb-port 50000 --irq-port 50001 &
-gdb-multiarch
+if $run_option
+then
+  ./arm_simulator --gdb-port 50000 --irq-port 50001 &
+  gdb-multiarch
+fi
+
+if ! $make_option && ! $run_option
+then
+  make distclean
+  ./configure CFLAGS='-Werror -Wall -g'
+  make
+  ./arm_simulator --gdb-port 50000 --irq-port 50001 &
+  gdb-multiarch
+fi
