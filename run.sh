4b6fb9973cd0bdef45f527afa1543255709ed57b
NielsTRS <trsniels@gmail.com>
2023-12-15 14:40:03 +0100

    ajout option -m pour make

--- run.sh
+++ run.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
-make distclean
-./configure CFLAGS='-Werror -Wall -g'
-make
+
+make_option=false
+
+while getopts ":m" opt; do
+  case $opt in
+    m)
+      make_option=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if $make_option
+then
+  make distclean
+  ./configure CFLAGS='-Werror -Wall -g'
+  make
+fi
+
 ./arm_simulator --gdb-port 50000 --irq-port 50001 &
 gdb-multiarch
