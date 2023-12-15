2ee5a512ec16492d4efaf73f7af1fe7e3ee52dab
corentin <cocoelabricot@gmail.Com>
2023-12-15 15:34:16 +0100

    edit gitignore (pas inclure makefile) + endian sur memory)
    z

--- memory.c
+++ memory.c
@@ -58,7 +58,7 @@ int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value, uint8_t be) {
     if(address < mem->size - 1 && mem != NULL){
-        *value = (uint16_t)mem->data[address] | (uint16_t)mem->data[address + 1] << 8;
+        *value = reverse_2((uint16_t)mem->data[address] | (uint16_t)mem->data[address + 1] << 8);
         return 0;
     }
     return -1;
@@ -66,36 +66,37 @@ int memory_read_half(memory mem, uint32_t address, uint16_t *value, uint8_t be) 
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value, uint8_t be) {
     if(address < mem->size - 3 && mem != NULL && mem->data != NULL){
-        *value = (uint16_t)mem->data[address] | (uint16_t)mem->data[address + 1] << 8 | (uint16_t)mem->data[address + 2] << 16 | (uint16_t)mem->data[address + 3] << 24;
+        *value = reverse_4((uint16_t)mem->data[address] | (uint16_t)mem->data[address + 1] << 8 | (uint16_t)mem->data[address + 2] << 16 | (uint16_t)mem->data[address + 3] << 24);
         return 0;
     }
     return -1;
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
-    if(address < mem->size && mem != NULL && mem->data != NULL){
-        (mem->data[address]) = value;
+    if(address < mem->size && mem != NULL){
+        *(mem->data + address) = value;
         return 0;
     }
     return -1;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value, uint8_t be) {
-    if(address < mem->size - 1 && mem != NULL && mem->data != NULL){
+    if(address < mem->size - 1 && mem != NULL){
+        value = reverse_2(value);
         mem->data[address] = (uint8_t)value;
-        mem->data[address+1] = (uint8_t)value << 8;
-
+        mem->data[address+1] = (uint8_t)(value >> 8);
         return 0;
     }
     return -1;
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value, uint8_t be) {
-    if(address < mem->size - 3 && mem != NULL && mem->data != NULL){
+    if(address < mem->size - 3 && mem != NULL){
+        value = reverse_4(value);
         mem->data[address] = (uint8_t)value;
-        mem->data[address+1] = (uint8_t)value << 8;
-        mem->data[address+2] = (uint8_t)value << 16;
-        mem->data[address+3] = (uint8_t)value << 24;
+        mem->data[address+1] = (uint8_t)(value >> 8);
+        mem->data[address+2] = (uint8_t)(value >> 16);
+        mem->data[address+3] = (uint8_t)(value >> 24);
         return 0;
     }
     return -1;
