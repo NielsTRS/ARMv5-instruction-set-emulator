47555b0dfa5c1ff6f8883b4e6faa3e9bb71b4390
corentin <cocoelabricot@gmail.Com>
2023-12-18 13:59:40 +0100

    arm_execute.c v1

--- arm_instruction.c
+++ arm_instruction.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -29,7 +29,56 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 
 static int arm_execute_instruction(arm_core p) {
-    return 0;
+    uint32_t *pc;
+    int result;
+    int instr;
+    uint8_t br;
+
+    result = arm_fetch(p, pc);
+    br = get_bits(*pc, 28, 4);
+    
+    if(br == 0x0D){ //cond = 1110(ALWAYS)
+        instr = switch_type(p, pc);
+    }
+    else if(br == 0x0F){ //cond = 1111 (cas chelou)
+        result = arm_miscellaneous(p, pc);
+    }
+    else{
+        result = arm_branch(p, pc);
+    }
+
+    
+
+    
+
+    return result;
+}
+
+int switch_type(arm_core p, uint32_t* pc){
+    int result;
+    uint8_t type;
+    type = get_bits(*pc, 25, 3);
+
+    switch (type){
+        case 0x00: //INSTR de type add r4, r3
+            result = arm_data_processing_shift(p, pc);
+            break;
+        case 0x01: //INSTR de type add r4, #5
+            result = arm_data_processing_immediate_msr(p, pc);
+            break;
+        case 0x02: //INSTR de type ldr r4, [r2, #5]
+            result = arm_load_store(p, pc);
+            break;
+        case 0x03: //INSTR de type ldr r4, [r2,r3]
+            result = arm_load_store_multiple(p, pc);
+            break;
+        case 0x05: //INSTR de type BL etiq
+            result = arm_branch(p, pc);
+            break;
+        default: 
+            result = -1; 
+            break; 
+    }
 }
 
 int arm_step(arm_core p) {
